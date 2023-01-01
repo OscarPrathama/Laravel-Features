f48df11cfb2ccda2ee586626bd8054d732ed52ba
masgan <oscarmasgan@gmail.com>
2023-01-01 20:12:42 +0700

    define Category, Menu, Tables, Reservation (Model, Controller, Migration)

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,12 @@
 <?php
 
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\MenuController;
+use App\Http\Controllers\Admin\ReservationController;
+use App\Http\Controllers\Admin\TableController;
 use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -24,15 +17,6 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    // Route::prefix('admin/users')->group(function () {
-    //     Route::get('/', [UserController::class, 'index'])->name('users.index');
-    //     Route::get('/create', [UserController::class, 'create'])->name('users.create');
-    //     Route::post('/', [UserController::class, 'store'])->name('users.store');
-    //     Route::get('/edit/{id}', [UserController::class, 'edit'])->name('users.edit');
-    //     Route::patch('/update/{id}', [UserController::class, 'update'])->name('users.update');
-    //     Route::delete('/delete/{id}', [UserController::class, 'destroy'])->name('users.delete');
-    // });
-
 });
 
 Route::middleware(['auth', 'admin'])->name('admin.')->prefix('admin')->group(function(){
@@ -42,6 +26,11 @@ Route::middleware(['auth', 'admin'])->name('admin.')->prefix('admin')->group(fun
     Route::get('/edit/{id}', [UserController::class, 'edit'])->name('users.edit');
     Route::patch('/update/{id}', [UserController::class, 'update'])->name('users.update');
     Route::delete('/delete/{id}', [UserController::class, 'destroy'])->name('users.delete');
+
+    Route::resource('/categories', CategoryController::class);
+    Route::resource('/menus', MenuController::class);
+    Route::resource('/tables', TableController::class);
+    Route::resource('/reservation', ReservationController::class);
 });
 
 require __DIR__.'/auth.php';
